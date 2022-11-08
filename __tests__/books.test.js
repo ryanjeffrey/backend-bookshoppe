641a8e68540cefaf12d55ec8e5d241c18a737b3a
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-11-08 14:44:59 -0600

    Add a test for books route

---
+++ __tests__/books.test.js
@@ -0,0 +1,19 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('books routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  it('should return a list of books', async () => {
+    const res = await request(app).get('/books');
+    expect(res.body.length).toEqual(6);
+  });
+
+  afterAll(() => {
+    pool.end();
+  });
+});
